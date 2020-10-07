50a9b64278bdc0736516566058eeb1f78216e0f6
Joshua Nathaniel M <clsjoshua@gmail.com>
2020-10-07 13:14:58 +0700

    First Commit

---
+++ deck.js
@@ -0,0 +1,9 @@
+import { slides as analysis } from './slides/02-databaseanalysis.mdx'
+import { slides as design } from './slides/03-databasedesign.mdx'
+import { slides as init } from './slides/01-introduction.mdx'
+
+export const slides = [
+  ...init,
+  ...analysis,
+  ...design
+]
